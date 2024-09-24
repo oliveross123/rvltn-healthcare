01b85bb15fbade620b710add197783a096b5d90a
oliveross123 <oliverstroch@seznam.cz>
2024-09-23 18:40:47 -0600

    implementace sentry.io

--- app/patients/[userId]/new-appointment/success/page.tsx
+++ app/patients/[userId]/new-appointment/success/page.tsx
@@ -6,6 +6,9 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
+import * as Sentry from "@sentry/nextjs";
+import { getUser } from "@/lib/actions/patient.actions";
+
 const Success = async ({
   params: { userId },
   searchParams,
@@ -35,6 +38,10 @@ const Success = async ({
   const doctor = Doctors.find(
     (doctor) => doctor.name === appointment.primaryPhysician
   );
+
+  const user = await getUser(userId);
+
+  Sentry.metrics.set("user_view_appointment-success", user.name);
 
   return (
     <div className="flex h-screen max-h-screen px-[5%]">
