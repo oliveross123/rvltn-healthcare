4057867073e77785d7c25e88dfe4d83cac1361e2
oliveross123 <oliverstroch@seznam.cz>
2024-11-14 14:31:01 -0600

    Tryout 3 , uprava "dashboard/appointemtns/page.tsx" z důvodu že appwrite vypovedel službu

--- app/dashboard/appointments/page.tsx
+++ app/dashboard/appointments/page.tsx
@@ -1,70 +1,76 @@
-import React from "react";
-import Link from "next/link";
-import Image from "next/image";
-import StatCard from "@/components/StatCard";
-import { getRecentAppointmentList } from "@/lib/actions/appointment.actions";
-import { DataTable } from "@/components/table/DataTable";
-import { columns } from "@/components/table/columns";
+// import React from "react";
+// import Link from "next/link";
+// import Image from "next/image";
+// import StatCard from "@/components/StatCard";
+// import { getRecentAppointmentList } from "@/lib/actions/appointment.actions";
+// import { DataTable } from "@/components/table/DataTable";
+// import { columns } from "@/components/table/columns";
 
-const Appointments = async () => {
-  const appointments = await getRecentAppointmentList();
+// const Appointments = async () => {
+//   const appointments = await getRecentAppointmentList();
 
-  return (
-    <div className="mx-auto flex max-w-7xl flex-col space-y-14">
-      <header className="admin-header">
-        <Link href="/" className="cursor-pointer">
-          {/* <Image
-            src="/assets/icons/logo-full.svg"
-            height={32}
-            width={162}
-            alt="Logo"
-            className="h-8 w-fit"
-          /> */}
+export default function Appointments() {
+  <div>
+    <h1>Kalendář nefunguje Appwrite vypovedel službu?!??!?! 🤬</h1>
+  </div>;
+}
 
-          <Link
-            href={"/"}
-            className="-mt-10 text-lg md:text-2xl font-semibold bg-gradient-to-br from-green-500 via-green-500 to-dark-500"
-          >
-            rvltnCare
-          </Link>
-        </Link>
+//   return (
+//     <div className="mx-auto flex max-w-7xl flex-col space-y-14">
+//       <header className="admin-header">
+//         <Link href="/" className="cursor-pointer">
+//           {/* <Image
+//             src="/assets/icons/logo-full.svg"
+//             height={32}
+//             width={162}
+//             alt="Logo"
+//             className="h-8 w-fit"
+//           /> */}
 
-        <p className="text-16-semibold">Přehled termínu</p>
-      </header>
+//           <Link
+//             href={"/"}
+//             className="-mt-10 text-lg md:text-2xl font-semibold bg-gradient-to-br from-green-500 via-green-500 to-dark-500"
+//           >
+//             rvltnCare
+//           </Link>
+//         </Link>
 
-      <main className="admin-main">
-        <section className="w-full space-y-4">
-          <h1 className="header">Vítejte 👋</h1>
-          <p className="text-dark-700">Začněte den kontrolou termínů</p>
-        </section>
+//         <p className="text-16-semibold">Přehled termínu</p>
+//       </header>
 
-        <section className="admin-stat">
-          <StatCard
-            type="naplánovat"
-            count={appointments.scheduledCount}
-            label="Naplánované termíny"
-            icon="/assets/icons/appointments.svg"
-          />
+//       <main className="admin-main">
+//         <section className="w-full space-y-4">
+//           <h1 className="header">Vítejte 👋</h1>
+//           <p className="text-dark-700">Začněte den kontrolou termínů</p>
+//         </section>
 
-          <StatCard
-            type="nevyřízene"
-            count={appointments.pendingCount}
-            label="Nevyřízene termíny"
-            icon="/assets/icons/pending.svg"
-          />
+//         <section className="admin-stat">
+//           <StatCard
+//             type="naplánovat"
+//             count={appointments.scheduledCount}
+//             label="Naplánované termíny"
+//             icon="/assets/icons/appointments.svg"
+//           />
 
-          <StatCard
-            type="zrušit"
-            count={appointments.cancelledCount}
-            label="Zrušené termíny"
-            icon="/assets/icons/cancelled.svg"
-          />
-        </section>
+//           <StatCard
+//             type="nevyřízene"
+//             count={appointments.pendingCount}
+//             label="Nevyřízene termíny"
+//             icon="/assets/icons/pending.svg"
+//           />
 
-        <DataTable columns={columns} data={appointments.documents} />
-      </main>
-    </div>
-  );
-};
+//           <StatCard
+//             type="zrušit"
+//             count={appointments.cancelledCount}
+//             label="Zrušené termíny"
+//             icon="/assets/icons/cancelled.svg"
+//           />
+//         </section>
 
-export default Appointments;
+//         <DataTable columns={columns} data={appointments.documents} />
+//       </main>
+//     </div>
+//   );
+// };
+
+// export default Appointments;
